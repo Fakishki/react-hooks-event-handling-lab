b336b50dee419d60911d6dad054eb549a418bb9d
Wally Pfingsten <wallypfingsten@gmail.com>
2023-03-10 17:07:58 -0800

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const passwordChange = () => {
+        console.log("Entering password...");
+    };
+
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password"
+                onChange={passwordChange}
+            />
+        </div>
     )
 }
 
